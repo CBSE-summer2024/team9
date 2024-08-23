1f9d8415d2f3ff593d39329c19e8923fe9e73b8e
Loai Masri <loai.masri@foothillsolutions.com>
2024-08-23 14:25:55 +0300

    Vue Project Setup #7: init vue project

---
+++ product-slider/src/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello From Vue</h1>
+</template>
---
+++ product-slider/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import ProductSlider from "./index.vue";
+
+createApp(ProductSlider).mount("#app");
